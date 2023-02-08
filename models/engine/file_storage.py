a56231c49dbc6fe6344615b2b6a3999d08ef1e88
PKiruri <107044946+PKiruri@users.noreply.github.com>
2023-02-08 11:51:27 +0300

    00

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,53 +1,69 @@
 #!/usr/bin/python3
-"""This is the File Storage module.
-Contains the FileStorage class.
-"""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
-from os import path
-
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-class FileStorage():
-    """This class serializes and deserializes instances to JSON and vice-versa.
-    Attributes:
-        __file_path (str): path to the JSON file.
-        __objects (dict): will store all objects by <class name>.id.
-    """
-
-    __file_path = "storage.json"
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns the dictionary __objects."""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            cls_objects = {}
+            for k, v in FileStorage.__objects.items():
+                if k.startswith(cls.__name__):
+                    cls_objects.update({k: v})
+            return cls_objects
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id.
-        Args:
-            obj: the object to set in.
-        """
-        FileStorage.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Serializes __objects to the JSON file"""
-        obj_dict = FileStorage.__objects.copy()
-        output = {k: v.to_dict() for k, v in obj_dict.items()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(output, f, sort_keys=True, indent=4)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f, sort_keys=True, indent=4)
+
+    def delete(self, obj=None):
+        """Deletes the object obj if obj is in __objects"""
+        if obj is not None:
+            for k, v in FileStorage.__objects.items():
+                if v is obj:
+                    tmp = k
+            FileStorage.__objects.pop(tmp)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
-            with open(FileStorage.__file_path, "r") as f:
-                input = json.load(f)
-                for k, v in input.items():
-                    FileStorage.__objects[k] = eval(v["__class__"])(**v)
-        except:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
             pass
+
+    def close(self):
+        """Handles storage close"""
+        self.reload()
